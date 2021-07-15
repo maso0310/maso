d08081c345b7e73a57daaad2b56f9c32d4ec6a26
maso0310 <balimon@hotmail.com>
2021-07-15 13:11:20 +0800

    'ok'

--- maso/__init__.py
+++ maso/__init__.py
@@ -1,11 +1,10 @@
 from supercv import *
+from superos import *
+from AI_model import *
+
 
 print('''
 功能列表：
-supercv:
-1.showimg_by_path(image_path):輸入圖片路徑直接看圖片
-2.showimg_by_ndarray(image):輸入圖片矩陣直接看圖片
+showimg_by_path(image_path):輸入圖片路徑直接看圖片
+showimg_by_ndarray(image):輸入圖片矩陣直接看圖片
 ''')
-
-def test():
-    print('這個是Maso專用的package')
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='maso',
-    version='1.0',
-    description='Make any image file to ascii art format.',
-    url='https://github.com/Henry0526/ascii_image',
+    version='1.1',
+    description='方便使用OPENCV包的套件',
+    url='https://github.com/maso0310/maso/tree/master',
     author='Maso',
     author_email='balimon@hotmail.com',
-    install_requires=["opencv-python", "numpy"],
-    license='MIT',
+    install_requires=["opencv-python", "numpy","joblib","pandas","scikit-learn==0.24.1"],
+    license='MIT License',
     packages=['maso'],
     zip_safe=False,
     keywords=['maso'],
